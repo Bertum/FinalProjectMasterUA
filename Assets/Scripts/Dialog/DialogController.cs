6620f0b7ce444da93b8b4eddd13319171fa809d6
Bertum <albertodelcru@gmail.com>
2019-07-28 13:17:09 +0200

    Language controller

---
+++ Assets/Scripts/Dialog/DialogController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogController : MonoBehaviour
+{
+    public GameObject DialogCanvas;
+    public Text DialogText;
+    private LanguageController languageController;
+
+    #region Singleton
+    public static DialogController instance
+    {
+        get
+        {
+            var gc = GameObject.FindObjectOfType<DialogController>();
+            if (gc == null)
+                throw new System.Exception("GameController not added to Scene");
+            return gc;
+        }
+    }
+    #endregion
+
+    private void Awake()
+    {
+        languageController = GameObject.FindObjectOfType<LanguageController>();
+        DialogCanvas.SetActive(false);
+        ShowDialog("menu.start");
+    }
+
+
+    public void ShowDialog(string idText)
+    {
+        DialogText.text = languageController.GetTextById(idText);
+        DialogCanvas.SetActive(true);
+    }
+
+    public void Accept()
+    {
+        DialogCanvas.SetActive(false);
+    }
+
+    public void Decline()
+    {
+        DialogCanvas.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Language/LanguageController.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LanguageController : MonoBehaviour
+{
+    private Dictionary<string, string> texts;
+    private string languageSelected;
+
+    private void Awake()
+    {
+        GetCurrentLanguage();
+    }
+
+    private void GetCurrentLanguage()
+    {
+        languageSelected = PlayerPrefs.HasKey(Constants.LANGUAGESELECTED) ? PlayerPrefs.GetString(Constants.LANGUAGESELECTED) : Application.systemLanguage.ToString();
+        var files = Resources.LoadAll<TextAsset>("Languages");
+        //If the selected or default language doesn't exists takes english as a default language
+        TextAsset selectedFile = files.Any(a => a.name == languageSelected) ? files.FirstOrDefault(f => f.name == languageSelected) : files.FirstOrDefault(f => f.name == SystemLanguage.English.ToString());
+        texts = JsonConvert.DeserializeObject<Dictionary<string, string>>(selectedFile.ToString());
+    }
+
+    /// <summary>
+    /// Get the text by id, if is not found return a default string
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public string GetTextById(string id)
+    {
+        string value = null;
+        if (!texts.TryGetValue(id, out value))
+        {
+            value = string.Format("Text with id {0} not found", id);
+        }
+        return value;
+    }
+}
