8041b5a7c13408ff51991e6866422cfc2c9f8f19
Bertum <albertodelcru@gmail.com>
2019-08-05 20:31:59 +0200

    Basic model builder

---
+++ Assets/Scripts/Dialog/DialogBuilder.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine.UI;
+
+public class DialogBuilder
+{
+    private DialogData data;
+
+    public DialogBuilder()
+    {
+        data = new DialogData();
+    }
+
+    public DialogData Build()
+    {
+        return data;
+    }
+
+    public void WithTextId(string textId)
+    {
+        data.TextId = textId;
+    }
+
+    public void WithImage(Image image)
+    {
+        data.Image = image;
+    }
+
+    public void WithCharacterName(string name)
+    {
+        data.CharacterName = name;
+    }
+
+    public void IsQuest()
+    {
+        data.IsQuest = true;
+    }
+
+    public void WithCharacter(string name, Image image)
+    {
+        data.CharacterName = name;
+        data.Image = image;
+    }
+}
--- Assets/Scripts/Dialog/DialogController.cs
+++ Assets/Scripts/Dialog/DialogController.cs
@@ -5,6 +5,7 @@ public class DialogController : MonoBehaviour
 {
     public GameObject DialogCanvas;
     public Text DialogText;
+    public Text CharacterName;
     public Image Image;
     private bool isQuest;
 
@@ -13,7 +14,7 @@ public class DialogController : MonoBehaviour
     {
         get
         {
-            var gc = GameObject.FindObjectOfType<DialogController>();
+            var gc = FindObjectOfType<DialogController>();
             if (gc == null)
                 throw new System.Exception("DialogController not added to Scene");
             return gc;
@@ -27,11 +28,12 @@ public class DialogController : MonoBehaviour
     }
 
 
-    public void ShowDialog(string idText, Image image, bool isQuest = false)
+    public void ShowDialog(DialogData data)
     {
-        DialogText.text = LanguageController.instance.GetTextById(idText);
-        Image = image;
-        this.isQuest = isQuest;
+        DialogText.text = LanguageController.instance.GetTextById(data.TextId);
+        Image = data.Image;
+        isQuest = data.IsQuest;
+        CharacterName.text = data.CharacterName;
         DialogCanvas.SetActive(true);
     }
 
