edff801e1d47792ce3aeee2753ccdccf78b3b149
Bertum <albertodelcru@gmail.com>
2019-08-06 22:40:51 +0200

    Fixed callbacks functions

--- Assets/Scripts/Dialog/DialogController.cs
+++ Assets/Scripts/Dialog/DialogController.cs
@@ -4,10 +4,13 @@ using UnityEngine.UI;
 public class DialogController : MonoBehaviour
 {
     public GameObject DialogCanvas;
-    public Text DialogText;
-    public Text CharacterName;
+    public Text DialogText, Title;
     public Image Image;
     private bool isQuest;
+    [HideInInspector]
+    public delegate void FunctionCallback();
+    [HideInInspector]
+    public FunctionCallback successFunction, declineFunction;
 
     #region Singleton
     public static DialogController instance
@@ -28,23 +31,30 @@ public class DialogController : MonoBehaviour
     }
 
 
-    public void ShowDialog(DialogData data)
+    public void ShowDialog(DialogData data, FunctionCallback success, FunctionCallback decline)
     {
+        successFunction = success;
+        declineFunction = decline;
         DialogText.text = LanguageController.instance.GetTextById(data.TextId);
         Image = data.Image;
         isQuest = data.IsQuest;
-        CharacterName.text = data.CharacterName;
+        Title.text = data.CharacterName;
         DialogCanvas.SetActive(true);
     }
 
     public void Accept()
     {
         //Save the quest if neccesary
+        successFunction();
+        successFunction?.Invoke();
+        successFunction = null;
         DialogCanvas.SetActive(false);
     }
 
     public void Decline()
     {
+        declineFunction?.Invoke();
+        declineFunction = null;
         DialogCanvas.SetActive(false);
     }
 }
---
+++ Assets/Scripts/Dialog/TestDialog.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class TestDialog : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DialogData data = new DialogBuilder().WithCharacterName("Paco").WithTextId("menu.start").Build();
+        DialogController.instance.ShowDialog(data, Test, Test);
+    }
+
+    public void Test()
+    {
+        Debug.Log("Hello");
+    }
+}
