64f7f612b72874c173b814183d454b688fe157d9
Bertum <albertodelcru@gmail.com>
2019-08-08 20:23:40 +0200

    Save Load service

---
+++ Assets/Scripts/PlayerData/PlayerData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class PlayerData
+{
+    public string PlayerName;
+    public Vector3 Position;
+    public Vector3 Rotation;
+    public string SceneName;
+}
---
+++ Assets/Scripts/PlayerData/PlayerDataController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerDataController : MonoBehaviour
+{
+    private SaveLoadService SaveLoadService;
+    public PlayerData PlayerData;
+    //Define if we can save, for example we cannot save in combat
+    public bool CanSave;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        CanSave = true;
+    }
+
+    public void Load()
+    {
+        PlayerData = SaveLoadService.Load();
+    }
+
+    public void Save()
+    {
+        if (CanSave)
+        {
+            //We will need to add logic here
+            SaveLoadService.Save(PlayerData);
+        }
+    }
+}
