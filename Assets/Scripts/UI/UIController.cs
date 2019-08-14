b153d1fccacb80eb5e18dc1940a18915c11f8da6
Bertum <albertodelcru@gmail.com>
2019-08-14 23:57:56 +0200

    UI Controller and Basic UI

---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Text goldText, foodText, piratesText;
+
+    public void ResourcesChanged(int currentGold, int totalGold, int currentFood, int totalFood, int currentPirates, int totalPirates)
+    {
+        goldText.text = string.Format("{0} / {1}", currentGold.ToString(), totalGold.ToString());
+        foodText.text = string.Format("{0} / {1}", currentFood.ToString(), totalFood.ToString());
+        piratesText.text = string.Format("{0} / {1}", currentPirates.ToString(), totalPirates.ToString());
+    }
+}
