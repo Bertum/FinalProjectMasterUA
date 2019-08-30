375870450bb3340b3827328465c04142d2cba825
Bertum <albertodelcru@gmail.com>
2019-08-30 21:10:13 +0200

    Animations properties changed to avoid move the position of characters

---
+++ Assets/Scripts/Characters/RandomSkinSelector.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class RandomSkinSelector : MonoBehaviour
+{
+    private void Awake()
+    {
+        var skins = GetComponentsInChildren<SkinnedMeshRenderer>();
+        var randomSkin = Random.Range(0, skins.Length - 1);
+        for (int i = 0; i < skins.Length; i++)
+        {
+            if (randomSkin != i)
+            {
+                skins[i].enabled = false;
+            }
+        }
+    }
+}
