559bac77aad4320629348ca28cf14d0c6ac5cd46
Bertum <albertodelcru@gmail.com>
2019-09-04 20:18:33 +0200

    Loading screen and check saved data

--- Assets/Scripts/MainMenu/MainMenuController.cs
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -4,11 +4,16 @@ using UnityEngine.UI;
 public class MainMenuController : MonoBehaviour
 {
     public Dropdown dropdown;
-
+    public GameObject btnContinue;
+    private SaveLoadService saveLoadService;
+    private SceneLoaderService sceneLoaderService;
 
     private void Awake()
     {
         dropdown.value = PlayerPrefs.HasKey(Constants.LANGUAGEINDEXSELECTED) ? PlayerPrefs.GetInt(Constants.LANGUAGEINDEXSELECTED) : 0;
+        saveLoadService = new SaveLoadService();
+        sceneLoaderService = new SceneLoaderService();
+        btnContinue.SetActive(saveLoadService.CheckExistSave());
     }
 
     public void LanguageChanged(Dropdown dropdown)
@@ -16,5 +21,18 @@ public class MainMenuController : MonoBehaviour
         PlayerPrefs.SetString(Constants.LANGUAGESELECTED, dropdown.captionText.text);
         PlayerPrefs.SetInt(Constants.LANGUAGEINDEXSELECTED, dropdown.value);
         LanguageController.instance.UpdateTexts();
+    }
+
+    public void LoadSavedGame()
+    {
+        if (saveLoadService.CheckExistSave())
+        {
+            sceneLoaderService.LoadScene(SceneNames.GAME);
+        }
+    }
+
+    public void LoadNewGame()
+    {
+        sceneLoaderService.LoadScene(SceneNames.GAME);
     }
 }
