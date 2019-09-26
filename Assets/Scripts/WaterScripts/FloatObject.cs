61e130d9a1727246fbe048018156b3aa742f08a2
Miguel <magudob@gmail.com>
2019-09-26 14:00:17 +0200

    shipMovementImproved

--- Assets/Scripts/WaterScripts/FloatObject.cs
+++ Assets/Scripts/WaterScripts/FloatObject.cs
@@ -10,18 +10,7 @@ public class FloatObject : MonoBehaviour {
     float forceFactor;
     Vector3 floatForce;
 
-   /* // Update is called once per frame
-    void FixedUpdate() {
-        forceFactor = 1.0f - ((transform.position.y - waterLevel) / floatThreshold);
-        //print(forceFactor);
-        if (forceFactor > 0.0f) {
-            floatForce = -Physics.gravity * GetComponent<Rigidbody>().mass * (forceFactor - GetComponent<Rigidbody>().velocity.y * waterDensity);            
-            floatForce += new Vector3(0.0f, -downForce * GetComponent<Rigidbody>().mass, 0.0f);
-            GetComponent<Rigidbody>().AddForceAtPosition(floatForce, transform.position);
-        }
-    }*/
-
-    private void FixedUpdate() {
+   private void FixedUpdate() {
         //Go Down
         if(transform.position.y > waterLevel + floatThreshold || transform.position.y > waterLevel - floatThreshold) {
             floatForce = new Vector3(0.0f, -downForce, 0.0f);
