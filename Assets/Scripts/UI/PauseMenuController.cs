05069c1f405348466d90d920262bf98f84f334fc
Bertum <albertodelcru@gmail.com>
2019-10-03 20:31:56 +0200

    Pause game

---
+++ Assets/Scripts/UI/PauseMenuController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public GameObject PauseMenu;
+    private bool paused;
+
+    private void Awake()
+    {
+        paused = false;
+        PauseMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseUnPause();
+        }
+    }
+
+    private void PauseUnPause()
+    {
+        paused = !paused;
+        PauseMenu.SetActive(paused);
+        Time.timeScale = paused ? 0 : 1;
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
