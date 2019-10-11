1e6e2c3930a987ade08a71b18cf5b8eb109dbb74
Bertum <albertodelcru@gmail.com>
2019-10-11 19:59:25 +0200

    Jobs text and UI

---
+++ Assets/Scripts/Player/MouseController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 1000))
+            {
+                Debug.Log(hit.transform.gameObject.name);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Shop/RecruitmentController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using static Constants;
+
+public class RecruitmentController : MonoBehaviour
+{
+    public GameObject npcToRecruit;
+    public GameObject shopList;
+    private PlayerDataController playerDataController;
+
+    void Awake()
+    {
+        playerDataController = GameObject.FindObjectOfType<PlayerDataController>();
+    }
+
+    public void Buy(EJobType type)
+    {
+    }
+
+    public void CreateList(List<EJobType> jobs)
+    {
+        foreach (var job in jobs)
+        {
+            var shopItem = Instantiate(npcToRecruit);
+
+            npcToRecruit.transform.parent = shopList.transform;
+        }
+    }
+}
