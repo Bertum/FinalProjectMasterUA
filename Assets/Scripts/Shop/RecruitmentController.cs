79cbcc4e811e42930a122dffec9512220d2aac5f
Bertum <albertodelcru@gmail.com>
2019-10-12 00:14:40 +0200

    Dynamic shop WIP

--- Assets/Scripts/Shop/RecruitmentController.cs
+++ Assets/Scripts/Shop/RecruitmentController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using static Constants;
 
 public class RecruitmentController : MonoBehaviour
@@ -13,6 +14,14 @@ public class RecruitmentController : MonoBehaviour
         playerDataController = GameObject.FindObjectOfType<PlayerDataController>();
     }
 
+    private void Start()
+    {
+        var list = new List<EJobType>() { EJobType.Carpenter, EJobType.Cook };
+        CreateList(list);
+    }
+
+
+
     public void Buy(EJobType type)
     {
     }
@@ -21,9 +30,15 @@ public class RecruitmentController : MonoBehaviour
     {
         foreach (var job in jobs)
         {
-            var shopItem = Instantiate(npcToRecruit);
-
-            npcToRecruit.transform.parent = shopList.transform;
+            var shopItem = Instantiate(npcToRecruit, shopList.transform);
+            var jobText = LanguageController.instance.GetTextById(string.Format("job.{0}", job.ToString().ToLower()));
+            shopItem.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = jobText;
+            //Stregnth
+            shopItem.transform.GetChild(0).GetChild(2).GetChild(1).GetComponent<Text>().text = "1";
+            //Dexterity
+            shopItem.transform.GetChild(0).GetChild(2).GetChild(3).GetComponent<Text>().text = "2";
+            //Intelligence
+            shopItem.transform.GetChild(0).GetChild(2).GetChild(5).GetComponent<Text>().text = "3";
         }
     }
 }
